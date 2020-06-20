085272bd2060cc33ad4d8a972c02e4095013c6bd
Joris De Winne <joris.dewinne@gmail.com>
2020-06-19 19:22:47 -0700

    Fixing unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('grid avatar', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const avatarElement = getByText(/Avatar/i);
+  expect(avatarElement).toBeInTheDocument();
 });
