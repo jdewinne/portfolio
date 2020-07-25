37ee206a509c7248cc77fd98607f86155653cdaf
Joris De Winne <joris.dewinne@gmail.com>
2020-07-24 19:06:36 -0700

    Adding light/dark toggle

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -30,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  link: {
+  appBarIcons: {
     padding: theme.spacing(1)
   },
   paper: {
@@ -40,24 +43,26 @@ const useStyles = makeStyles((theme) => ({
   },
   appBar: {
     color: 'inherit',
-    background: theme.palette.grey[800],
+    background: theme.palette.type === "dark" ? theme.palette.grey[800] : '',
   },
 }));
 
-function svgEmoji (input) {
-	return emoji(input, {
-		baseUrl: 'https://twemoji.maxcdn.com/2/svg/',
-		ext: '.svg',
+function svgEmoji(input) {
+  return emoji(input, {
+    baseUrl: 'https://twemoji.maxcdn.com/2/svg/',
+    ext: '.svg',
     size: '',
     props: {
-      style: {height: 1.5 + 'em'}
+      style: { height: 1.5 + 'em' }
     }
-	})
+  })
 }
 
 
-function App() {
+function App({theme, onToggleDark}) {
   const classes = useStyles();
+
+  let themeButton = theme.palette.type === "light" ? <Brightness4Icon /> : <Brightness7Icon />;
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
@@ -68,38 +73,43 @@ function App() {
             </Link>
           </Box>
           <Divider orientation="vertical" flexItem />
-          
+
           <Box component="span" m={1}>
             <Link href="https://www.equalityfederation.org/" color="inherit">
-              { svgEmoji('🏳️‍🌈', "lgbtq", ) }
+              {svgEmoji('🏳️‍🌈', "lgbtq",)}
             </Link>
           </Box>
           <Box component="span" m={1}>
             <Link href="https://blacklivesmatter.com/" color="inherit">
-              { svgEmoji('✊🏽', "blm", ) }
+              {svgEmoji('✊🏽', "blm",)}
             </Link>
           </Box>
           <Box component="span" m={1}>
             <Link href="https://www.syriacivildefence.org/en/" color="inherit">
-              <img alt="White Helmets" src="images/WhiteHelmetsIcon.jpg" style={{height:1.5 + 'em'}} />
+              <img alt="White Helmets" src="images/WhiteHelmetsIcon.jpg" style={{ height: 1.5 + 'em' }} />
             </Link>
           </Box>
           <Typography className={classes.root} align="right">
-            <Link href="http://github.com/jdewinne" color="inherit" className={classes.link}>
+            <Link href="http://github.com/jdewinne" color="inherit" className={classes.appBarIcons}>
               <GitHubIcon />
             </Link>
-            <Link href="http://www.linkedin.com/in/jorisdewinne" color="inherit" className={classes.link}>
+            <Link href="http://www.linkedin.com/in/jorisdewinne" color="inherit" className={classes.appBarIcons}>
               <LinkedInIcon />
             </Link>
-            <Link href="mailto:joris@dewinne.dev" color="inherit" className={classes.link}>
+            <Link href="mailto:joris@dewinne.dev" color="inherit" className={classes.appBarIcons}>
               <EmailIcon />
             </Link>
-            <Link href="tel:+16266899527" color="inherit" className={classes.link}>
+            <Link href="tel:+16266899527" color="inherit" className={classes.appBarIcons}>
               <PhoneIcon />
             </Link>
-            <Link href="pdf/Joris_De_Winne_Resume.pdf" color="inherit" className={classes.link} download target="_self">
+            <Link href="pdf/Joris_De_Winne_Resume.pdf" color="inherit" className={classes.appBarIcons} download target="_self">
               <PictureAsPdfIcon />
             </Link>
+            <Tooltip title="Toggle light/dark theme">
+              <Link color="inherit" className={classes.appBarIcons} onClick={onToggleDark}>
+                {themeButton}
+              </Link>
+            </Tooltip>
           </Typography>
         </Toolbar>
       </AppBar>
