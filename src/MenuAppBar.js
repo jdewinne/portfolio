1619c9b7467a8c0fed18441b4a25747c4d0ed007
Joris De Winne <joris.dewinne@gmail.com>
2020-07-29 11:50:38 -0700

    Created separate component for the app bar menu

--- src/MenuAppBar.js
+++ src/MenuAppBar.js
@@ -6,6 +6,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Avatar from '@material-ui/core/Avatar';
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -16,8 +18,10 @@ import Brightness7Icon from '@material-ui/icons/Brightness7';
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MenuIcon from '@material-ui/icons/Menu';
 import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 import PhoneIcon from '@material-ui/icons/Phone';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 import emoji from 'react-easy-emoji';
 
@@ -32,6 +36,10 @@ const useStyles = makeStyles((theme) => ({
   appBarIcons: {
     padding: theme.spacing(1)
   },
+  drawerPaper: {
+    top: '56px',
+    height: 'auto',
+  }
 }));
 
 function svgEmoji(input) {
@@ -45,20 +53,59 @@ function svgEmoji(input) {
   })
 }
 
-function MenuAppBar({theme, onToggleDark}) {
+function MenuAppBar({ theme, onToggleDark }) {
   const classes = useStyles();
+  const [drawerLeftOpen, setDrawerLeftOpen] = React.useState(false);
+  const [drawerRightOpen, setDrawerRightOpen] = React.useState(false);
+
+  const toggleLeftDrawer = () => {
+    setDrawerLeftOpen(!drawerLeftOpen);
+  };
+
+  const toggleRightDrawer = () => {
+    setDrawerRightOpen(!drawerRightOpen);
+  };
 
   let themeButton = theme.palette.type === "light" ? <Brightness4Icon /> : <Brightness7Icon />;
   return (
-    <AppBar position="static" className={classes.appBar}>
-      <Toolbar variant="dense">
-        <Box component="span" m={1}>
-          <Link href="https://dewinne.dev" color="inherit">
-            <Avatar alt="Joris De Winne" src="images/avatar.png" />
-          </Link>
-        </Box>
-        <Divider orientation="vertical" flexItem />
-
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleLeftDrawer}>
+            <MenuIcon />
+          </IconButton>
+          <Divider orientation="vertical" flexItem />
+          <Box component="span" m={1}>
+            <Link href="https://dewinne.dev" color="inherit">
+              <Avatar alt="Joris De Winne" src="images/avatar.png" />
+            </Link>
+          </Box>
+          <Typography className={classes.root} align="right">
+            <IconButton aria-label="settings" edge="end" color="inherit" onClick={toggleRightDrawer}>
+              <SettingsIcon />
+            </IconButton>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={drawerLeftOpen} onClose={toggleLeftDrawer} classes={{ paper: classes.drawerPaper, }} >
+        <Link href="http://github.com/jdewinne" color="inherit" className={classes.appBarIcons}>
+          <GitHubIcon />
+        </Link>
+        <Link href="http://www.linkedin.com/in/jorisdewinne" color="inherit" className={classes.appBarIcons}>
+          <LinkedInIcon />
+        </Link>
+        <Divider />
+        <Link href="mailto:joris@dewinne.dev" color="inherit" className={classes.appBarIcons}>
+          <EmailIcon />
+        </Link>
+        <Link href="tel:+16266899527" color="inherit" className={classes.appBarIcons}>
+          <PhoneIcon />
+        </Link>
+        <Divider />
+        <Link href="pdf/Joris_De_Winne_Resume.pdf" color="inherit" className={classes.appBarIcons} download target="_self">
+          <PictureAsPdfIcon />
+        </Link>
+        <Divider />
         <Box component="span" m={1}>
           <Link href="https://www.equalityfederation.org/" color="inherit">
             {svgEmoji('🏳️‍🌈', "lgbtq")}
@@ -74,30 +121,15 @@ function MenuAppBar({theme, onToggleDark}) {
             <img alt="White Helmets" src="images/WhiteHelmetsIcon.jpg" style={{ height: 1.5 + 'em' }} />
           </Link>
         </Box>
-        <Typography className={classes.root} align="right">
-          <Link href="http://github.com/jdewinne" color="inherit" className={classes.appBarIcons}>
-            <GitHubIcon />
+      </Drawer>
+      <Drawer anchor="right" open={drawerRightOpen} onClose={toggleRightDrawer} classes={{ paper: classes.drawerPaper, }} >
+        <Tooltip title="Toggle light/dark theme">
+          <Link color="inherit" className={classes.appBarIcons} onClick={onToggleDark}>
+            {themeButton}
           </Link>
-          <Link href="http://www.linkedin.com/in/jorisdewinne" color="inherit" className={classes.appBarIcons}>
-            <LinkedInIcon />
-          </Link>
-          <Link href="mailto:joris@dewinne.dev" color="inherit" className={classes.appBarIcons}>
-            <EmailIcon />
-          </Link>
-          <Link href="tel:+16266899527" color="inherit" className={classes.appBarIcons}>
-            <PhoneIcon />
-          </Link>
-          <Link href="pdf/Joris_De_Winne_Resume.pdf" color="inherit" className={classes.appBarIcons} download target="_self">
-            <PictureAsPdfIcon />
-          </Link>
-          <Tooltip title="Toggle light/dark theme">
-            <Link color="inherit" className={classes.appBarIcons} onClick={onToggleDark}>
-              {themeButton}
-            </Link>
-          </Tooltip>
-        </Typography>
-      </Toolbar>
-    </AppBar>
+        </Tooltip>
+      </Drawer>
+    </div >
   );
 }
 
