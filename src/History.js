0576a13baded9e2b027b843a759277f2d7faa5ba
Joris De Winne <joris.dewinne@gmail.com>
2020-10-29 11:20:19 -0700

    Adding Python Functions, Files, and Dictionaries certificate

--- src/History.js
+++ src/History.js
@@ -87,6 +87,12 @@ function History() {
                 <ListItem>
                     <ListItemIcon><VerifiedUserIcon /></ListItemIcon>
                     <ListItemText>
+                        Python Functions, Files, and Dictionaries: <Link href="https://coursera.org/share/77c65b18b61692a1ae6734703966667c" className={classes.link} underline='always'>Coursera - University of Michigan</Link> | October 2020
+                    </ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon><VerifiedUserIcon /></ListItemIcon>
+                    <ListItemText>
                         Python Basics: <Link href="https://coursera.org/share/9a49f54a6d84f6f5d7a2ef4aa92cacf8" className={classes.link} underline='always'>Coursera - University of Michigan</Link> | October 2020
                     </ListItemText>
                 </ListItem>
